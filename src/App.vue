c7b4c0037c084941eaed10524b87de833778fa60
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-02-27 19:47:38 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,18 @@ import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useToast } from 'vue-toastification';
 
 const toast = useToast();
-const transactions = ref([
-  { id: 1, text: 'Flower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 299.97 },
-  { id: 3, text: 'Groceries', amount: -150 },
-  { id: 4, text: 'Freelance Work', amount: 500 },
-]);
-
+const transactions = ref([]);
+onMounted(() => {
+  // Load transactions from localStorage if available
+  const storedTransactions = localStorage.getItem('transactions');
+  if (storedTransactions) {
+    transactions.value = JSON.parse(storedTransactions);
+  }
+});
 // Calculate total balance
 const total = computed(() => {
   return transactions.value.reduce(
@@ -55,6 +56,7 @@ const handleTransactionSubmitted = (newTransaction) => {
     id: generateUniqueId(),
     ...newTransaction,
   });
+  saveTransactionsToLocalStorage();
   toast.success('Transaction added successfully!');
 };
 
@@ -63,7 +65,12 @@ const deleteTransaction = (id) => {
   transactions.value = transactions.value.filter(
     (transaction) => transaction.id !== id,
   );
+  saveTransactionsToLocalStorage();
   toast.success('Transaction deleted successfully!');
+};
+// Save transactions to localStorage whenever they change
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value));
 };
 // Generate unique ID for transactions(helper function)
 function generateUniqueId() {
